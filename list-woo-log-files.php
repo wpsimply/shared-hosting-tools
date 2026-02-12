b5f38b4b8b79f666698c8554e65afc9ff2e55d76
Viktor Szépe <viktor@szepe.net>
2026-02-12 08:00:13 +0000

    Add list-woo-log-files.php

---
+++ list-woo-log-files.php
@@ -0,0 +1,17 @@
+<?php
+
+// wp eval-file list-woo-log-files.php
+
+$log_files = array_filter(
+    WC_Log_Handler_File::get_log_files(),
+    static function ($log_file) {
+        return fnmatch('*-'.date('Y-m-d').'-*.log', $log_file);
+    }
+);
+
+echo implode("\n", array_map(
+    static function ($log_file) {
+        return Automattic\WooCommerce\Utilities\LoggingUtil::get_log_directory().$log_file;
+    },
+    $log_files
+));
