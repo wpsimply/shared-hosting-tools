28c3cd98470186bab2c35c5cb46b165e3e669b31
Viktor Szépe <viktor@szepe.net>
2026-03-03 20:27:53 +0000

    Add Apache-like access.log

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,25 @@
 <?php
 
+// Write an Apache-like access.log
+register_shutdown_function(function () {
+    file_put_contents(
+        dirname(__DIR__).'/access.log',
+        sprintf(
+            '%s - - [%s] "%s %s %s" %d %.2f "%s" "%s"' . PHP_EOL,
+            $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1',
+            date('d/M/Y:H:i:s O'),
+            $_SERVER['REQUEST_METHOD'] ?? 'GET',
+            $_SERVER['REQUEST_URI'] ?? '/',
+            $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.1',
+            http_response_code(),
+            sys_getloadavg()[0] ?? 0, // CPU load
+            $_SERVER['HTTP_REFERER'] ?? '-',
+            $_SERVER['HTTP_USER_AGENT'] ?? '-'
+        ),
+        FILE_APPEND | LOCK_EX
+    );
+});
+
 // Run WP-Cron from linux crontab
 define('DISABLE_WP_CRON', true);
 
